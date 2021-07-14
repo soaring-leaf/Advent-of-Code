ae117371b03a6b3a2b95ae544e3f3363f8ced151
Soaring Leaf <cyclist02@gmail.com>
2021-07-14 19:16:38 -0400

    Test example working

--- 2020/Day14/docking.pl
+++ 2020/Day14/docking.pl
@@ -12,12 +12,13 @@ open(INPUT,"<","test.txt") or die "Cant' open Test.txt $!";
 
 while(<INPUT>) {
 	chomp;
-	(my $input, my $value) = split(' = ');
+	my ($input, $value) = split(' = ');
 
 	if ($input eq 'mask') {
 		$mask = $value;
 	} else {
 		$memLoc = substr($input,4,-1);
+		$value = convertValue($mask,$value);
 		$mem{$memLoc} = $value;
 	}
 }
@@ -25,7 +26,7 @@ while(<INPUT>) {
 close(INPUT);
 
 foreach my $m (keys (%mem)) {
-	print "key is $m\n";
+	print "key is $m, value is now: $mem{$m}\n";
 	$total += $mem{$m};
 }
 
@@ -33,3 +34,15 @@ print "Sum of all values in Memory: $total\n";
 
 exit(0);
 #==========================================================================
+sub convertValue {
+	my ($m, $val) = @_;
+
+	my $bin = sprintf ("%.36b", $val);
+
+	while(index($m,"X") != -1) {
+		my $i = index($m,"X");
+		substr($m,$i,1,substr($bin,$i,1));
+	}
+	
+	return oct("0b".$m);
+}
