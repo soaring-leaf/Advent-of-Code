a5aa8b7e855a032103aba3c296d47c5fde146845
Soaring Leaf <cyclist02@gmail.com>
2021-07-17 15:32:25 -0400

    working in part 2 for 2020 turns

--- 2020/Day15/numberGame.pl
+++ 2020/Day15/numberGame.pl
@@ -34,13 +34,12 @@ print "The 2020th number: $sequence[0]\n";
 # =================================================================
 
 my %numMap = (); # need hash for faster lookup
-my $num = 0;
 
 # reset vars
 @sequence = (13,0,10,12,1,5,8);
 @test = (0,3,6);
 $check = 0;
-$turn = 1;
+$turn = 0;
 
 # Init turnMap with starting numbers
 for(my $i=0;$i<scalar(@test);$i++) {
@@ -60,32 +59,40 @@ $turn++;
 # print $numMap{$check}[1] . "\n";
 # confirmed above is working as expected!
 
-=begin
-while ($turn <= 10) {
+while ($turn < 2020) {
+    print "turn: $turn: number from last turn = $check\n";
+
     # last number said from previous turn must exist
     # if last number exists but only has one entry, it's only been said once
         # next number = 0
     if(@{$numMap{$check}} == 1) { # should $num be consolidated into $check???
         # update 0 in the hashMap, it already has two entries
         $check = 0;
-        $numMap{$check}[1] = $numMap{$check}[0];
-        $numMap{$check}[0] = $turn;
+        $numMap{$check}[0] = $numMap{$check}[1];
+        $numMap{$check}[1] = $turn;
     } else {
         # if last number has more than 1 entry
         # calc next number to be said
-        $num = $numMap{$check}[0] - $numMap{$check}[1];
-        # if new number exists in hash, then need to update the hash for that num
-            # if this is the second occurrence, need to push current turn into array
-            # else update values: move [0] to [1] and update [0] with new value
+        print "map($check) [0,1]: " . $numMap{$check}[0] . ", " . $numMap{$check}[1] . "\n";
+        $check = $numMap{$check}[1] - $numMap{$check}[0];
         # if !exists in hash, add new entry for next number
+        # if new number exists in hash, then need to update the hash for that num
+        if(!exists($numMap{$check})) {
+            my @newTracker = ($turn);
+            $numMap{$check} = \@newTracker;
+        } elsif(@{$numMap{$check}} == 1) {
+            # if this is the second occurrence, need to push current turn into array
+            push(@{$numMap{$check}},$turn);
+        } else {
+            # else update values: move [0] to [1] and update [0] with new value
+            $numMap{$check}[0] = $numMap{$check}[1];
+            $numMap{$check}[1] = $turn;
+        }
     }
     $turn++;
 }
-=cut
 
-$turn--;
-
-print "Number for turn $turn is $num \n";
+print "Number for turn $turn is $check \n";
 
 exit(0);
 #==========================================================================
