a19f654a5cfc8166714bdcb4f13bbbc1a7b9ba0e
Soaring Leaf <cyclist02@gmail.com>
2021-07-17 13:48:28 -0400

    Part 1 solution

--- 2020/Day15/numberGame.pl
+++ 2020/Day15/numberGame.pl
@@ -7,26 +7,26 @@ my @test = (6,3,0);
 my $check = 0;
 my $found = 0;
 
-while(scalar(@test) < 10) {
-    $check = $test[0];
-    for(my $i=1;$i<scalar(@test);$i++) {
-        if($test[$i] == $check) {
-            unshift(@test,$i);
+while(scalar(@sequence) < 2020) {
+    $check = $sequence[0];
+    for(my $i=1;$i<scalar(@sequence);$i++) {
+        if($sequence[$i] == $check) {
+            unshift(@sequence,$i);
             $found++;
-            $i = scalar(@test);
+            $i = scalar(@sequence);
         }
     }
 
     if(!$found) {
-        unshift(@test,0);
+        unshift(@sequence,0);
     } else {
         $found = 0;
     }
 
-    print "Next number added is: $test[0]\n";
+    #print "Next number added is: $test[0]\n";
 }
 
-print "The 2020th number: $test[0]\n";
+print "The 2020th number: $sequence[0]\n";
 
 exit(0);
 #==========================================================================
