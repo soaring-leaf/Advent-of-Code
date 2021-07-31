f93c2557cd4b9102f6af9c36e1109780584535db
Soaring Leaf <cyclist02@gmail.com>
2021-07-31 16:25:27 -0400

    Input Extraction complete for Pt2

--- 2020/Day16/trainTickets.pl
+++ 2020/Day16/trainTickets.pl
@@ -6,6 +6,12 @@ my $section = 0; # Section of input currently being read
 my $err = 0; # Tracks Ticket Error Rate of invalid numbers
 my $max = 0; # Max valid number
 my $min = -1; # Min valid number - ranges overlap so only need one max/min set
+my $invalid = 0; # flag for a valid/invalid ticket
+my %fieldOptions = (); # hash to hold the ticket field data
+my %validTickets = (); # hash to hold valid tickets
+my $ticketCount = 0; # ticket counter for hash key
+my @myTicket = (); # my ticket details
+
 
 open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
 
@@ -14,7 +20,11 @@ while(<INPUT>) {
         $section++;
     } elsif ($section == 0) {
         my ($label,$ranges) = split(': ');
-        my ($currMin,$junk,$currMax) = split('-',$ranges);
+        my ($currMin,$mid,$currMax) = split('-',$ranges);
+        my ($range1Max,$range2Min) = split(' or ',$mid);
+
+        my @dataRanges = ($currMin,$range1Max,$range2Min,$currMax);
+        $fieldOptions{$label} = \@dataRanges;
 
         if($min == -1 || $min > $currMin) {
             $min = $currMin;
@@ -24,22 +34,36 @@ while(<INPUT>) {
             $max = $currMax;
         }
     } elsif($section == 1) {
-        # Part 1 - ignore my ticket so nothing to do here
+        @myTicket = split(',');
     } else {
         my @currTicket = split(',');
 
         if(scalar(@currTicket) > 1) {
+            # tabulate Errors in Ticket Scanning
             for (my $i=0;$i<scalar(@currTicket);$i++) {
                 if($currTicket[$i] < $min || $currTicket[$i] > $max) {
                     $err += $currTicket[$i];
+                    $invalid++;
                 }
+            }
+            
+            # if it's a valid ticket, add it to the valid ticket hash
+            if(!$invalid) {
+                $validTickets{$ticketCount} = \@currTicket;
+                $ticketCount++;
             }
         }
     }
+
+    $invalid = 0; # reset invalid ticket flag
 }
 
 close(INPUT);
 
+$ticketCount--; # fix ticket count for notes
+print "There are $ticketCount valid tickets. \n";
+
+# Part 1 answer:
 print "Ticket scanning Error Rate is $err \n";
 
 exit(0);
