995539f019b95633086d4f1baad7f2eae88e893d
Soaring Leaf <cyclist02@gmail.com>
2021-12-11 12:12:54 -0500

    2020-Day16-Part2 framework

--- 2020/Day16/trainTickets.pl
+++ 2020/Day16/trainTickets.pl
@@ -10,15 +10,16 @@ my $invalid = 0; # flag for a valid/invalid ticket
 my %fieldOptions = (); # hash to hold the ticket field data
 my %validTickets = (); # hash to hold valid tickets
 my $ticketCount = 0; # ticket counter for hash key
+my $totalTickets = 0; # Total Tickets in Input
 my @myTicket = (); # my ticket details
 
 
 open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
 
 while(<INPUT>) {
-    if(length($_) < 5) {
+    if(length($_) < 5) { # Blank line, on to next section
         $section++;
-    } elsif ($section == 0) {
+    } elsif ($section == 0) { # fields and values
         my ($label,$ranges) = split(': ');
         my ($currMin,$mid,$currMax) = split('-',$ranges);
         my ($range1Max,$range2Min) = split(' or ',$mid);
@@ -33,13 +34,14 @@ while(<INPUT>) {
         if ($max < $currMax) {
             $max = $currMax;
         }
-    } elsif($section == 1) {
+    } elsif($section == 1) { # My Ticket info
         @myTicket = split(',');
-    } else {
+    } else { # All the other ticket values
         my @currTicket = split(',');
 
         if(scalar(@currTicket) > 1) {
-            # tabulate Errors in Ticket Scanning
+            $totalTickets++;
+            # tabulate Errors in Ticket Scanning (Part 1)
             for (my $i=0;$i<scalar(@currTicket);$i++) {
                 if($currTicket[$i] < $min || $currTicket[$i] > $max) {
                     $err += $currTicket[$i];
@@ -60,11 +62,14 @@ while(<INPUT>) {
 
 close(INPUT);
 
-$ticketCount--; # fix ticket count for notes
-print "There are $ticketCount valid tickets. \n";
+print "There are $ticketCount valid tickets out of $totalTickets. \n";
 
 # Part 1 answer:
 print "Ticket scanning Error Rate is $err \n";
+
+# Part 2: Use built hashes to figure out which field is which
+my %currPossible = (); # hash for current possible options the field could be
+my @solution = (); # array identifying the field position
 
 exit(0);
 #==========================================================================
