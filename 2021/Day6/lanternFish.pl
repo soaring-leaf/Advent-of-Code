723d34f662113d9a86e2e29b1a6fa21b2491d515
Soaring Leaf <cyclist02@gmail.com>
2022-03-24 18:06:28 -0400

    2021-Day6 Part 2 complete

--- 2021/Day6/lanternFish.pl
+++ 2021/Day6/lanternFish.pl
@@ -3,7 +3,8 @@ use warnings;
 use strict;
 
 my @fishBuckets = ((0) x 9); # Buckets for the fish in each day
-my $total80 = 0; # Final count of fish
+my $total80 = 0; # Final count of fish after 80 days
+my $total256 = 0; # Final count after 256 days
 
 open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
 #open(INPUT,"<","testInput.txt") or die "Can't open Test Inptu file $!";
@@ -21,7 +22,8 @@ while(<INPUT>) {
 
 close(INPUT);
 
-for(my $d = 0; $d < 80; $d++) {
+# Part 1: 1 - 80 days
+for(my $p1 = 0; $p1 < 80; $p1++) {
     my $temp = $fishBuckets[0];
 
     # process a day in the lanternfish life
@@ -38,11 +40,28 @@ foreach my $b (@fishBuckets) {
     $total80 += $b;
 }
 
+# Part 2: Day 81 - 256
+# continue doing the same thing up to day 256
+for(my $p2 = 80; $p2 < 256; $p2++) {
+    my $temp = $fishBuckets[0];
+
+    for(my $f2=0; $f2 < scalar(@fishBuckets) - 1; $f2++) {
+        $fishBuckets[$f2] = $fishBuckets[$f2+1];
+    }
+
+    $fishBuckets[6] += $temp;
+    $fishBuckets[8] = $temp;
+}
+
+foreach my $b2 (@fishBuckets) {
+    $total256 += $b2;
+}
+
 # Part 1 answer:
-print "Total fish after 80 days is: $total80\n";
+print "Total fish after 256 days is: $total80\n";
 
 # Part 2 answer:
-#print "\n\n";
+print "Total fish after 80 days is: $total256\n\n";
 
 exit(0);
 #==========================================================================
