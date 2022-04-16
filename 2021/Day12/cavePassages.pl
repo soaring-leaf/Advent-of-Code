bf79752ea6b34a690834ed707bd00118a106673b
Soaring Leaf <cyclist02@gmail.com>
2022-04-16 12:29:36 -0400

    2021-Day12 outline to recursivly traverse the cave

--- 2021/Day12/cavePassages.pl
+++ 2021/Day12/cavePassages.pl
@@ -51,9 +51,17 @@ while(<INPUT>) {
 
 close(INPUT);
 
-# figure out how to traverse the cave starting with 'start'
-# need way to determine if little cave has already been visited
-# if successful in reaching end, +1 to pathCount
+# sort each connection array so 'end' is last (desc)
+foreach my $k (keys %caveConnections) {
+    my @sorted = sort {lc($b) cmp lc($a)} @{$caveConnections{$k}};
+    $caveConnections{$k} = \@sorted;
+    print "$k: ";
+    printPath($caveConnections{$k}); # connections list
+}
+
+print "\n\n";
+
+$pathCount = traverseCave(\%caveConnections,\%roomSize,['start']);
 
 # Part 1 answer:
 print "There are $pathCount paths through the cave.\n\n";
@@ -63,3 +71,29 @@ print "\n\n";
 
 exit(0);
 #==========================================================================
+sub printPath {
+    foreach my $e (@{$_[0]}) {
+        print "$e ";
+    }
+    print "\n";
+}
+
+# Recursive function to find a path through the cave given a starting room and
+# the current path already taken. 
+# If End is reached return 1, if no path is available, return 0
+sub traverseCave {
+    my $pCount = 0; # count of paths calculated
+    my $roomHashRef = $_[0]; # reference to the room hash
+    my $roomSizeRef = $_[1]; # ref to the Room Size hash
+    my @currPath = @{$_[2]}; # Current path already taken
+    my $currRoom = $currPath[scalar(@currPath)-1]; # current Room
+
+    # for each connection in the current room:
+    # 1. check size and if small room, see if it's already been visited
+    # 2. if so, skip it and check next one
+    # 3. if ok to visit, push it on the current Path array and recurse (add returned result to path Count)
+    # 4. if no more rooms are available to visit, return 0 (can't reach exit)
+    # 5. if 'end' is found, print path and return 1
+
+    return $pCount;
+}
