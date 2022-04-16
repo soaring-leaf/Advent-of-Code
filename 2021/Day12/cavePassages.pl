a6ed837116b6e5bdfaa6b61da5c85cd05c75cd1d
Soaring Leaf <cyclist02@gmail.com>
2022-04-15 21:45:24 -0400

    2021-Day12 structure for cave map built

--- 2021/Day12/cavePassages.pl
+++ 2021/Day12/cavePassages.pl
@@ -3,6 +3,7 @@ use warnings;
 use strict;
 
 my %caveConnections; # List of all the passages between rooms in cave
+my %roomSize; # List of the rooms and if they are Big (1) or little (0)
 my $pathCount = 0; # count of viable paths
 
 open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
@@ -11,10 +12,41 @@ open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
 while(<INPUT>) {
     chomp;
     my ($first, $second) = split('-',$_);
+
+    if($first eq uc($first)) {
+        $roomSize{$first} = 1;
+    } else {
+        $roomSize{$first} = 0;
+    }
+    
+    if($second eq uc($second)) {
+        $roomSize{$second} = 1;
+    } else {
+        $roomSize{$second} = 0;
+    }
+
+    # Start is one way out, no need to build paths FROM End
     
     # if first time seeing room, create array and add to hash for path
-    # else add path to hash array
-    # do for both directions unless Start/End is present
+    # otherwise, add the room to the array
+    if($first ne 'end' && $second ne 'start') {
+        if(!exists($caveConnections{$first})) {
+            my @newPath = ($second);
+            $caveConnections{$first} = \@newPath;
+        } else { 
+            push(@{$caveConnections{$first}},$second);
+        }
+    }
+
+    # do for both directions
+    if($first ne 'start' && $second ne 'end') {
+        if(!exists($caveConnections{$second})) {
+            my @otherPath = ($first);
+            $caveConnections{$second} = \@otherPath;
+        } else {
+            push(@{$caveConnections{$second}},$first);
+        }
+    }
 }
 
 close(INPUT);
