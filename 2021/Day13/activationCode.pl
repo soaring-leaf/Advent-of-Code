244c0ea40a86cca4d0d3896177d9132233370e09
Soaring Leaf <cyclist02@gmail.com>
2022-04-18 10:59:47 -0400

    2021-Day13 Part 1 complete

--- 2021/Day13/activationCode.pl
+++ 2021/Day13/activationCode.pl
@@ -2,11 +2,10 @@
 use warnings;
 use strict;
 
-my @marks; # locations of all the marks on the paper from input
+my %marks; # locations of all the marks on the paper from input
 my @instr; # instructions on where to fold the paper
 my $inputGroup = 0; # flag indicating what part of input is being read - 0 for marks, 1 for instr
 my $markCount = 0; # count of marks on the whole paper
-my $marksShowing = 0; # count of marks showing after the first fold
 my $maxX = 0; # Largest X dimension
 my $maxY = 0; # Largest Y dimension
 
@@ -19,7 +18,7 @@ while(<INPUT>) {
     if(length($_) < 1) {
         $inputGroup++;
     } elsif($inputGroup == 0) {
-        push(@marks,$_);
+        $marks{$_} = 0;
         $markCount++;
 
         # find largest x and y coords
@@ -43,32 +42,37 @@ close(INPUT);
 print "There are $markCount marks. \n";
 print "Width of paper (x): $maxX\n";
 print "Height of paper (y): $maxY\n";
-print "Additionally, there are " . scalar(@instr) . " fold instructions:\n";
+print "Additionally, there are " . scalar(@instr) . " fold instructions.\n\n";
 
-foreach my $i (@instr) {
-    print "$i\n";
-}
-print "\n";
-
-# Build the paper matrix (Y coord = Row, X coord = Column)
-    # start with all 0's of ROW length = X (in Y rows)
-    # go through @marks and put a '#' on any coord with a mark
-    # remember to swap the order when placing marks - X coord is the COLUMN
-    # X coord is the COLUMN!!!
-    # X coord is the COLUMN!!!
-    # X coord is the COLUMN!!!
-# Fold the paper once - X Fold: coord distance to fold (minus 1) on X becomes the new X coord
-    # If 1 unit away from fold, new X coord is 0
-    # If 11 units away, new coord is 10
-    # If first coord has a mark, mark the new coord
-    # If the second also has a mark, just mark it again in the same way so no duplication
-# Count the marks showing on the paper (represented by a new matrix, half the size)
+# First fold (Part1)
+foldPaper(split('=',shift(@instr)),\%marks);
 
 # Part 1 answer:
-print "After the first Fold, there are $marksShowing marks showing.\n\n";
+print "After the first Fold, there are " . scalar(keys(%marks)) . " marks showing.\n\n";
 
 # Part 2 answer:
 #print "\n\n";
 
 exit(0);
 #==========================================================================
+
+# Folding the paper - 
+    #First half of paper doesn't change coords, 
+    # second half subtracts the distance to the fold value twice
+sub foldPaper {
+    my ($dir, $fCoord, $markHashRef) = @_;
+
+    foreach my $m (keys %{$markHashRef}) {
+        my ($x, $y) = split(',',$m);
+
+        if($dir eq 'x' && $x > $fCoord) {
+            delete($markHashRef->{$m});
+            $x = $x - ($x - $fCoord) * 2;
+            $markHashRef->{"$x,$y"} = 0;
+        } elsif($dir eq 'y' && $y > $fCoord) {
+            delete($markHashRef->{$m});
+            $y = $y - ($y - $fCoord) * 2;
+            $markHashRef->{"$x,$y"} = 0;
+        }
+    }
+}
