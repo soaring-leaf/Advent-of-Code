9f5d271bc34f092e02ffcafd2ecd69d5f01d272a
Soaring Leaf <cyclist02@gmail.com>
2022-12-04 17:35:53 -0500

    2022 Day4 complete

--- 2022/Day4/campCleanup.pl
+++ 2022/Day4/campCleanup.pl
@@ -2,68 +2,35 @@
 use warnings;
 use strict;
 
-my $overLap = 0; # count of overlapping cleaning assignments
-my @assigns; # Array to hold each assignment pair
+my $fullOverLap = 0; # count of overlapping cleaning assignments
+my $partOverLap = 0; # Part 2 - count of any overlap
 
 open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
 
 while(<INPUT>) {
     chomp;
 
-    push(@assigns,$_);
-    
     my ($e1,$e2) = split(',');
 
     my ($e1St, $e1Fin) = split('-',$e1);
     my ($e2St, $e2Fin) = split('-',$e2);
     
+    # Part 1 - full overlap
     if(($e1St >= $e2St && $e1Fin <= $e2Fin) || ($e2St >= $e1St && $e2Fin <= $e1Fin)) {
-        $overLap++;
+        $fullOverLap++;
+    }
+
+    # Part 2 - Partial overlap
+    if($e1St <= $e2Fin && $e1Fin >= $e2St) {
+        $partOverLap++;
     }
 }
 
 close(INPUT);
 
-print "There are $overLap Elves that have overlapping areas.\n\n";
+print "There are $fullOverLap Elves that have fully overlapping areas.\n";
 
-# Part 2 - 
+print "There are $partOverLap assignments that partially overlap.\n\n";
 
 exit(0);
 #==========================================================================
-sub getCommonItem {
-    my ($c1,$c2) = @_;
-
-    my @check = split('',$c1);
-
-    for(my $i=0;$i<scalar(@check);$i++) {
-        if(index($c2,$check[$i]) >= 0) {
-            return $check[$i];
-        }
-    }
-
-    return '-';
-}
-
-sub getBadge {
-    my ($b1,$b2,$b3) = @_;
-
-    my @bCheck = split('',$b1);
-
-    for(my $i=0;$i<scalar(@bCheck);$i++) {
-        if(index($b2,$bCheck[$i]) >= 0 && index($b3,$bCheck[$i]) >= 0) {
-            return $bCheck[$i];
-        }
-    }
-
-    return '-';
-}
-
-sub getPriority {
-    my $c = $_[0];
-    my $p = ord(lc($c)) - 96;
-
-    if(ord($c) < 91) {
-        $p += 26
-    }
-    return $p;
-}
