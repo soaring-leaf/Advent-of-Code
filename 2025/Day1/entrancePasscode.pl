96e4b855ca3c13d8ea886d272ba6401f629add97
Soaring Leaf <cyclist02@gmail.com>
2025-12-19 12:42:10 -0500

    2025 Day1 Part 2 complete

--- 2025/Day1/entrancePasscode.pl
+++ 2025/Day1/entrancePasscode.pl
@@ -2,20 +2,29 @@
 use warnings;
 use strict;
 
-my $dialPointerPt1 = 50; # Starting point of Dial
+my $dialPointer = 50; # Starting point of Dial
 my $zeroCountPt1 = 0; # Counts times dial stops at 0
+my $zeroCountPt2 = 0; # Counts times dial stops on or passes 0
 
 open(INPUT,"<","input.txt") or die "Can't open Input.txt $!";
+#open(INPUT,"<","inputTest.txt") or die "Can't open InputTest.txt $!";
 
 while(<INPUT>) {
     chomp;
     my $direction = substr($_,0,1);
     my $clicksPt1 = substr($_,1) % 100;
+    my $clicksPt2 = substr($_,1);
+    my $rotatePast0Count = int($clicksPt2/100);
+    
+    $zeroCountPt2 += $rotatePast0Count;
 
-    $dialPointerPt1 = calcNextNumber($dialPointerPt1,$direction,$clicksPt1);
+    $zeroCountPt2 += doesDialPass0($dialPointer,$direction,$clicksPt2 % 100);
 
-    if($dialPointerPt1 == 0) {
+    $dialPointer = calcNextNumber($dialPointer,$direction,$clicksPt1);
+
+    if($dialPointer == 0) {
         $zeroCountPt1++;
+        $zeroCountPt2++;
     }
 }
 
@@ -24,9 +33,9 @@ close(INPUT);
 # Part 1 - Output Result
 print "Part 1 - Times Dial stops at 0: " . $zeroCountPt1 . "\n";
 
-# Part 2 - 
+# Part 2 - Output Result
 
-#print "Part 2 - " . $x . "\n";
+print "Part 2 - Times Dial stops on or passes 0: " . $zeroCountPt2 . "\n";
 
 exit(0);
 #==========================================================================
@@ -46,4 +55,27 @@ sub calcNextNumber {
     }
 
     return $dial;
+}
+
+sub doesDialPass0 {
+    my($dial, $dir, $count) = @_;
+
+    if($dial == 0) {
+        return 0; # doesn't count if it Starts at 0
+    }
+
+    if($dir eq 'L') {
+        if($dial < $count) {
+            return 1;
+        } else {
+            return 0;
+        }
+        
+    } else {
+        if($dial + $count > 100) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
